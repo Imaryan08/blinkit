0076ef7404ceb900ca79f4a7795c56a8a3c518e3
Pranav Void <pranavvoid@pm.me>
2022-02-24 13:24:31 +0530

    three payment methods done

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,3 +4,33 @@ document.querySelectorAll('.payment-options__tabs > li').forEach((e) => {
         e.classList.add('payment-options__tabs--selected');
     });
 });
+
+
+document.querySelector('#payment-option-wallet').addEventListener('click', () => {
+    document.querySelector('.payment-option-display__shown').classList.remove('payment-option-display__shown');
+    document.querySelector('#payment-wallets').classList.add('payment-option-display__shown');
+});
+
+
+document.querySelector('#payment-option-upi').addEventListener('click', () => {
+    document.querySelector('.payment-option-display__shown').classList.remove('payment-option-display__shown');
+    document.querySelector('#payment-upi').classList.add('payment-option-display__shown');
+});
+
+
+document.querySelector('#payment-option-card').addEventListener('click', () => {
+    document.querySelector('.payment-option-display__shown').classList.remove('payment-option-display__shown');
+    document.querySelector('#payment-card').classList.add('payment-option-display__shown');
+});
+
+
+document.querySelector('#payment-option-cash').addEventListener('click', () => {
+    document.querySelector('.payment-option-display__shown').classList.remove('payment-option-display__shown');
+    document.querySelector('#payment-placeholder').classList.add('payment-option-display__shown');
+});
+
+
+document.querySelector('#payment-option-net-banking').addEventListener('click', () => {
+    document.querySelector('.payment-option-display__shown').classList.remove('payment-option-display__shown');
+    document.querySelector('#payment-placeholder').classList.add('payment-option-display__shown');
+});
