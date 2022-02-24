b737964107567b0c0feac4712c0a1d4ae6221f66
Pranav Void <pranavvoid@pm.me>
2022-02-24 19:29:57 +0530

    initial commit

---
+++ scripts/cart.js
@@ -0,0 +1,28 @@
+  const slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('cart'),
+    'padding': 360,
+    'side': 'right',
+  });
+
+  // Toggle button
+  document.querySelector('#btn-toggle-cart').addEventListener('click', () => {
+      slideout.toggle();
+  });
+
+function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });
