f4344549ab103541f798c959ced55ced17700c0e
Rampukar Yadav <imaryan08@gmail.com>
2022-02-26 17:28:28 +0530

    brandlinks in footer changed
    all the brandlinks have active links now

--- components/footer.js
+++ components/footer.js
@@ -13,8 +13,8 @@ let footer = () => {
                 <div class="col"><p class="mt-2">We will send you a link, open it on your phone to download the app</p></div>
             </div>
             <div class="row mt-4">
-                <div class="col"><input type="radio" name="userContact" value="email"> email</div>
-                <div class="col"><input type="radio" name="userContact" value="phone"> phone</div>
+                <div class="col"><input type="radio" name="" value="email"> email</div>
+                <div class="col"><input type="radio" name="" value="phone"> phone</div>
                 <div class="col"></div>
                 <div class="col"></div>
                 <div class="col"></div>
@@ -219,122 +219,8 @@ let footer = () => {
         </div>
     </div>
     <div class="row">
-        <div class="col brands">
-            <a href="">
-                grofers Happy Day
-grofers Happy Home
-grofers Mother's Choice
-g Fresh
-O'range
-Savemore
-24 Mantra
-Aashirvaad
-Act II
-Amul
-Axe
-Bambino
-Best Value
-Bingo
-Bisleri
-Boost
-Bournvita
-Britannia
-Brooke Bond
-Bru
-Cadbury
-Cheetos
-Cinthol
-Closeup
-Coca-Cola
-Colgate
-Dabur
-Danone
-Del Monte
-Dettol
-Dhara
-Dove
-Durex
-English Oven
-Everest
-Fiama Di Wills
-Garnier
-Gatorade
-Gillette
-Glucon-D
-Grocery
-Gowardhan
-Hajmola
-Haldiram's
-Head & Shoulders
-Heinz
-Himalaya
-Horlicks
-India Gate
-Kellogg's
-Kinley
-Kissan
-Knorr
-L'Oreal
-Lay's
-Lijjat
-Limca
-Lipton
-Maggi
-Madhur
-McCain
-MDH
-Minute Maid
-Mirinda
-Mother Dairy
-Mountain Dew
-MTR
-Nescafe
-Nestle
-Nivea
-Nutella
-Oral-B
-Oreo
-Palmolive
-Pantene
-Paper Boat
-Parachute
-Parle
-Patanjali
-Pears
-Pepsi
-Pepsodent
-Pillsbury
-Princeware
-Rajdhani
-Real
-Red Bull
-Safal
-Saffola
-Shakti Bhog
-Smith & Jones
-Sprite
-Stayfree
-Sundrop
-Sunfeast
-Sunsilk
-Taj Mahal
-Tang
-Tata sampann
-Tata tea
-Tetley
-Thums Up
-Tropicana
-Twinings
-Uncle Chipps
-Unibic
-Vaseline
-Veet
-Wagh Bakri
-Wai Wai
-Whisper
-Whole Farm
-
-            </a>
+        <div class="col brands brandLinks">
+            
         </div>
     </div>
 </div>
@@ -380,4 +266,8 @@ Whole Farm
     `;
 }
 
-export default footer;
+
+
+
+export {footer};
+
