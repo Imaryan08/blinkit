c6966f1b2da719e7c96c3d59b9260f0f39e96984
Rampukar Yadav <imaryan08@gmail.com>
2022-04-03 03:14:39 +0530

    profile modal updated

--- scripts/newProfile.js
+++ scripts/newProfile.js
@@ -1,3 +1,5 @@
+wallet();
+mobile();
 function order() {
     window.location.href = "order.html";
 }
@@ -7,5 +9,24 @@ function address() {
 }
 
 function wallet() {
-    window.location.href = "wallet.html";
+    // window.location.href = "wallet.html";
+    let walletAmount = localStorage.getItem('inputForAmt');
+    console.log('walletAmount:', walletAmount)
+    document.querySelector('.walletAmount').innerHTML = '₹' + walletAmount;
+}
+
+function logout() {
+    let isLoggedIn = localStorage.getItem('isLoggedIn');
+    isLoggedIn = false;
+    localStorage.setItem('isLoggedIn', isLoggedIn);
+    window.location.href = "index.html";
+}
+
+function faq() {
+    window.location.href = "https://blinkit.com/faq";
+}
+
+function mobile(){
+    let mobile = localStorage.getItem('mobile');
+    document.querySelector('#mobile').innerHTML = mobile;
 }
